f723e17498c8e600b4d35687084c5124c4fd3775
camporredonchinoyo <camporredonchinoyo@hotmail.com>
2014-01-13 22:20:55 +0100

    modificacion para el equals

--- parent-pom/model/src/main/java/es/microforum/model/Empleado.java
+++ parent-pom/model/src/main/java/es/microforum/model/Empleado.java
@@ -53,6 +53,31 @@ public class Empleado implements java.io.Serializable {
 		this.imagen = imagen;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+
 	@Id
 	@Column(name = "dni", unique = true, nullable = false, length = 45)
 	public String getDni() {
--- parent-pom/model/src/main/java/es/microforum/model/Empresa.java
+++ parent-pom/model/src/main/java/es/microforum/model/Empresa.java
@@ -5,6 +5,7 @@ package es.microforum.model;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -24,6 +25,31 @@ public class Empresa implements java.io.Serializable {
 
 	private String nif;
 	private Integer version;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nif == null) ? 0 : nif.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empresa other = (Empresa) obj;
+		if (nif == null) {
+			if (other.nif != null)
+				return false;
+		} else if (!nif.equals(other.nif))
+			return false;
+		return true;
+	}
+
 	private String nombre;
 	private String direccionFiscal;
 	private Date fechaInicioActividades;
