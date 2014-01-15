3ff348d7254123106c7a09bfc15911050b994e73
camporredonchinoyo <camporredonchinoyo@hotmail.com>
2014-01-15 19:28:46 +0100

    cambiada la validacion del equals

--- parent-pom/model/src/test/java/es/microforum/model/EmpleadoTest.java
+++ parent-pom/model/src/test/java/es/microforum/model/EmpleadoTest.java
@@ -2,14 +2,28 @@ package es.microforum.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class EmpleadoTest {
+	
+	Empleado empleado1;
+	Empleado empleado2;
+
+	@Before
+	public void setUp() throws Exception {
+		empleado1= new Empleado("71134014m");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 
 	@Test
 	public void test() {
-		Empleado empleado = new Empleado("71134014m");
-		assertTrue(empleado.getDni().equals("71134014m"));
+		empleado2 = new Empleado("71134014m");
+		assertTrue(empleado1.equals(empleado2));
 	}
 
 }
--- parent-pom/model/src/test/java/es/microforum/model/EmpresaTest.java
+++ parent-pom/model/src/test/java/es/microforum/model/EmpresaTest.java
@@ -2,14 +2,28 @@ package es.microforum.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class EmpresaTest {
+	
+	Empresa empresa1;
+	Empresa empresa2;
+	
+	@Before
+	public void setUp() throws Exception {
+		 empresa1 = new Empresa("B12344567");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 
 	@Test
 	public void test() {
-		Empresa empresa = new Empresa("B12344567");
-		assertTrue(empresa.getNif().equals("B12344567"));
+		empresa2  = new Empresa("B12344567");
+		assertTrue(empresa1.equals(empresa2));
 	}
 
 }
